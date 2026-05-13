0cd4668bad0d7dacc9935407712e352db7be425d
TecXCTO <cto.tecx@gmail.com>
2026-05-13 15:01:13 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@ pkg install termux-api
 
 # nano dj.sh
 
-chmod +x dj.sh
-./dj.sh
+chmod +x shell/dj.sh
+./shell/dj.sh
 
 # nano pulse.sh
-chmod +x pulse.sh
-./pulse.sh
+chmod +x shell/pulse.sh
+./shell/pulse.sh
 
 pkg install termux-api tool-types -y
 
@@ -15,5 +15,8 @@ pkg install termux-api tool-types -y
 # nano sound_dj.sh
 
 
-chmod +x sound_dj.sh
-./sound_dj.sh
+chmod +x shell/sound_dj.sh
+./shell/sound_dj.sh
+
+chmod +x run_py_code.sh
+./run_py_code.sh
