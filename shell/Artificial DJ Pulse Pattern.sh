be73d8827124cfda77875f867784f5a7aad311f7
TecXCTO <cto.tecx@gmail.com>
2026-05-13 14:49:40 +0530

    Create Artificial DJ Pulse Pattern.sh

---
+++ shell/Artificial DJ Pulse Pattern.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "DJ Music Pulsing is active... Press Ctrl+C to stop."
+
+# Turn off the flashlight automatically on exit
+trap "termux-torch off; exit" INT
+
+while true; do
+    # Array of random flash durations
+    SPEEDS=(0.04 0.08 0.15 0.05 0.2 0.03)
+    RANDOM_SPEED=${SPEEDS[$RANDOM % ${#SPEEDS[@]}]}
+    
+    termux-torch on
+    sleep $RANDOM_SPEED
+    termux-torch off
+    
+    # Array of random pause gaps between flashes
+    GAPS=(0.05 0.1 0.3 0.05 0.2)
+    RANDOM_GAP=${GAPS[$RANDOM % ${#GAPS[@]}]}
+    sleep $RANDOM_GAP
+done
