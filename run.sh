bf8b78c0907d2874b9a19a522f59ae1a1e69e850
TecXCTO <cto.tecx@gmail.com>
2026-05-13 14:45:40 +0530

    Update run.sh to execute pulse.sh and sound_dj.sh
    Added execution permissions and execution for pulse.sh and sound_dj.sh.

---
+++ run.sh
@@ -0,0 +1,19 @@
+pkg install termux-api
+
+# nano dj.sh
+
+chmod +x dj.sh
+./dj.sh
+
+# nano pulse.sh
+chmod +x pulse.sh
+./pulse.sh
+
+pkg install termux-api tool-types -y
+
+
+# nano sound_dj.sh
+
+
+chmod +x sound_dj.sh
+./sound_dj.sh
